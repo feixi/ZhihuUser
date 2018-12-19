b4bb868bb0a3c76692a24e7564207cab0e254279
LemonBottom <1255679669@qq.com>
2018-12-20 03:21:28 +0800

    Initial commit

---
+++ zhihuUser/pipelines.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import pymongo
+
+
+class ZhihuuserPipeline(object):
+
+    def __init__(self, mongo):
+        cli = pymongo.MongoClient(*mongo)
+        self.collection = cli.zhihu.userInfo
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings.getlist("MONGO"))
+
+    def process_item(self, item, spider):
+        self.collection.insert(dict(item))
+        return item
---
+++ zhihuUser/spiders/user.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+import re
+from ..items import ZhihuuserItem
+from scrapy_redis.spiders import RedisCrawlSpider
+
+
+class UserSpider(RedisCrawlSpider):
+    name = 'user'
+    allowed_domains = ['www.zhihu.com']
+    redis_key = "myspider:start_urls"
+    # start_urls = ['https://www.zhihu.com/api/v4/members/su-fei-17/followers?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=0&limit=20']
+
+    def parse(self, response):
+        temp_data = json.loads(response.body.decode("utf-8"))['data']
+        for u in temp_data:
+            item = ZhihuuserItem(
+                name=u['name'],
+                url_token=u['url_token'],
+                headline=u['headline'],
+                follower_count=u['follower_count'],
+                answer_count=u['answer_count'],
+                articles_count=u['articles_count'],
+                uid=u['id'],
+                gender=u['gender'],
+                type=u['type']
+            )
+            yield item
+            new_user_url = f'https://www.zhihu.com/api/v4/members/{u["url_token"]}/followers?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=0&limit=20'
+            # print("new_user: ",new_user_url)
+            yield scrapy.Request(new_user_url)
+        if len(temp_data) == 20:
+            old_offset = re.findall("offset=(\d+)&", response.url)[0]
+            new_offset = str(int(old_offset) + 20)
+            new_offset_url = response.url.replace(f"offset={old_offset}&", f"offset={new_offset}&")
+            # print("new_offset: ", new_offset_url)
+            yield scrapy.Request(new_offset_url)
