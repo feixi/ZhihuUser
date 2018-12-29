46370f148cab1d73f3eebfe4fbc90154f4308a6d
LemonBottom <1255679669@qq.com>
2018-12-29 22:11:16 +0800

    添加布隆过滤器

--- zhihuUser/pipelines.py
+++ zhihuUser/pipelines.py
@@ -6,17 +6,20 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import pymongo
+from zhihuUser.BloomFilter import BloomFilter
 
 
 class ZhihuuserPipeline(object):
 
     def __init__(self, mongo):
         self.collection = pymongo.MongoClient(*mongo).zhihu.user
+        self.f = BloomFilter("BloomFilter_zhihu")
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler.settings.getlist("MONGO"))
 
     def process_item(self, item, spider):
-        self.collection.insert(dict(item))
+        if not self.f.contains(item['url_token']):
+            self.collection.insert(dict(item))
         return item
